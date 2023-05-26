deeb513b4275efc90c069579b41254d7af2b7ac6
Monica Dao <monicatdao@gmail.com>
2023-05-26 04:58:22 -0700

    continuous road

---
+++ Flipflapflop/Assets/Scripts/GroundSpawn.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSpawn : MonoBehaviour
+{
+    public GameObject Template;
+    public GameObject TemplateEmpty;
+    public GameObject SpawnReserver;
+    private float DistanceTravelled = 0;
+
+    private void Start()
+    {
+        GameObject Spawned = Instantiate(TemplateEmpty, SpawnReserver.transform);
+        Spawned.transform.parent = transform;
+
+        SpawnReserver.transform.position += new Vector3(6.8f, 0, 0);
+        GameObject Spawned1 = Instantiate(TemplateEmpty, SpawnReserver.transform);
+        Spawned1.transform.parent = transform;
+
+        SpawnReserver.transform.position += new Vector3(6.8f, 0, 0);
+        GameObject Spawned2 = Instantiate(TemplateEmpty, SpawnReserver.transform);
+        Spawned2.transform.parent = transform;
+
+        SpawnReserver.transform.position += new Vector3(6.8f, 0, 0);
+        GameObject Spawned3 = Instantiate(TemplateEmpty, SpawnReserver.transform);
+        Spawned3.transform.parent = transform;
+
+        SpawnReserver.transform.position += new Vector3(6.8f, 0, 0);
+        GameObject Spawned4 = Instantiate(TemplateEmpty, SpawnReserver.transform);
+        Spawned4.transform.parent = transform;
+    }
+
+    public void Update()
+    {
+        transform.position += new Vector3(-3 * Time.deltaTime, 0, 0);
+        // Debug.Log("positionx: " + transform.position.x + " .... distance: " + DistanceTravelled);
+        // Debug.Log(transform.position.x - DistanceTravelled);
+        if (Math.Abs(transform.position.x) - Math.Abs(DistanceTravelled) >= 6.8)
+
+        {
+            DistanceTravelled = transform.position.x;
+            GameObject Spawned = Instantiate(Template, SpawnReserver.transform);
+            Spawned.transform.parent = transform;
+        }
+    }
+}
